fc74937399c6e71d90cfb7e1243d0128142627a4
Esan Taiwo <145122266+tmeechh@users.noreply.github.com>
2026-01-18 20:43:14 +0100

    done

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ import adminRoutesV1 from './src/v1/routes/admin.routes.js';
 dotenv.config();
 
 const app = express();
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true })); 
